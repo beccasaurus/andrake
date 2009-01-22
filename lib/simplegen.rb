77b6d08e07c6cf765348d66080724096eca5dd29
remi <remi@remitaylor.com>
2009-01-22 09:42:51 -0700

    started adding basic 'filters' ... really just procs

--- lib/simplegen.rb
+++ lib/simplegen.rb
@@ -1,21 +1,37 @@
 $:.unshift File.dirname(__FILE__)
+require 'fileutils'
 require 'rubygems'
 
 class SimpleGen
   class << self
+
+    # paths to look for templates in
     attr_accessor :template_directories
+    
+    # filtering engines for templates, eg. ERB
+    #
+    # these should be procs (or something callable)
+    #
+    # see SimpleGen::Filter
+    #
+    # this is a Hash, key = the filter name, which will be used for the file extension, eg. 'erb'
+    attr_accessor :filters
 
     def templates
-      puts "template_directories => #{ template_directories.inspect }"
       template_directories.inject([]) do |all_templates, template_dir|
         dirs = Dir[File.join(template_dir, '*')].select {|x| File.directory? x }
         all_templates += dirs.map {|dir| SimpleGen::Template.new(dir) }
         all_templates
       end
     end
+
+    def [] template_name
+      templates.find {|t| t.name == template_name.to_s }
+    end
   end
 end
 
+SimpleGen.filters              ||= {}
 SimpleGen.template_directories ||= []
 
 # move to simplegen/template.rb
@@ -31,4 +47,60 @@ class SimpleGen::Template
     File.basename root
   end
 
+  def generate! root_path_for_generation = '.', variables = { }
+    puts files_and_directories.inspect
+    puts files.inspect
+  end
+
+  protected
+
+  # returns an Array of all of the files and directories
+  # in this SimpleGen::Template's #root directory
+  #
+  # currently ignores hidden files and folders
+  #
+  def files_and_directories
+    Dir[ File.join(root, '**', '*') ]
+  end
+
+  def files
+    files_and_directories.select {|x| File.file? x }
+  end
+
+  def directories
+    files_and_directories.select {|x| File.directory? x }
+  end
+
 end
+
+# move to 'simplegen/filter.rb'
+#
+# note, a filter doesn't have to be a SimpleGen::Filter, it can be 
+# a Proc, it just needs to respond to #call
+class SimpleGen::Filter
+
+  attr_accessor :proc
+
+  def initialize &block
+    @proc = block
+  end
+
+  def call text, binding = TOPLEVEL_BINDING
+    @proc.call text, binding
+  end
+
+  # include an ERB filter, out of the box, as it comes with the ruby standard library
+  def self.erb_filter
+    # at the moment, there's no reason why this isn't just a simple proc
+    # ... we might do something fancy with SimpleGen::Filter at some point ...
+    # really, though, things should just be simple procs for now
+    SimpleGen::Filter.new do |text, binding|
+      require 'erb'
+      ERB.new(text).result(binding)
+    end
+  end
+
+end
+
+# add ERB filter by default, if not defined
+SimpleGen.filters[:erb] = SimpleGen::Filter.erb_filter unless SimpleGen.filters.keys.include?(:erb)
--- spec/simeplgen/simplegen_spec.rb
+++ spec/simeplgen/simplegen_spec.rb
@@ -15,8 +15,23 @@ describe SimpleGen do
     File.join template_dir(template_dir_name), name.to_s
   end
 
+  # the root directory where we'll be generating stuff to test
+  def generation_path_root
+    root = File.join File.dirname(__FILE__), '..', '..'
+    path = File.join root, 'examples', 'simplegen', 'generate_here'
+    File.expand_path path
+  end
+
+  # gets the path where we'll generate stuff
+  def generation_path name = 'generated-by-specs'
+    File.join generation_path_root, name.to_s
+  end
+
   before do
     SimpleGen.template_directories = []
+    Dir[ File.join(generation_path_root, '*') ].select {|x| File.directory? x }.each do |generated_dir|
+      FileUtils.rm_f generated_dir
+    end
   end
 
   it 'should read available generators from template directories' do
@@ -27,8 +42,34 @@ describe SimpleGen do
     SimpleGen.templates.should_not be_empty
     SimpleGen.templates.first.should be_a_kind_of(SimpleGen::Template)
     SimpleGen.templates.map {|x| x.name }.should include('app')
+    app_template = SimpleGen.templates.find {|t| t.name == 'app' }
+    SimpleGen[:app].root.should == app_template.root # helper shortcut, if helpful
   end
 
-  it "should generate 'app' correctly"
+  it "should generate 'app' correctly" do
+    SimpleGen.template_directories << template_dir # template-dir1
+    File.exist?(File.join(generation_path, 'foo.xml')).should be_false
+    SimpleGen[:app].generate! generation_path, :var => "RUBY VARIABLE"
+    #File.exist?(File.join(generation_path, 'foo.xml')).should be_true
+    #File.read(File.join(generation_path, 'foo.xml')).should include('hello from RUBY VARIABLE')
+  end
+
+  it 'filter should work properly' do
+    # ruby comes with ERB so we should always have it defined
+    SimpleGen.filters.keys.should include(:erb)
+    SimpleGen.filters[:erb].should be_a_kind_of(SimpleGen::Filter)
+  end
+
+  it 'should know how to render a file ... and we should have SimpleGen::Filters' do
+    #SimpleGen.template_directories << template_dir # template-dir1
+    #SimpleGen[:app].files.should_not be_empty
+    #SimpleGen[:app].files.first.should be_a_kind_of(SimpleGen::Template::File)
+    #SimpleGen[:app].files.first.filename.should == 'foo.xml._erb'
+    #SimpleGen[:app].files.first.path.should include('app/foo.xml.erb')
+    #SimpleGen[:app].files.first.filename_to_generate.should == 'foo.xml'
+    #SimpleGen[:app].files.first.filters.length.should == 1
+    #SimpleGen[:app].files.first.filters.first.should be_a_kind_of(SimpleGen::Filter)
+    #SimpleGen[:app].files.first.filters.first.name.downcase.should == 'erb'
+  end
 
 end
