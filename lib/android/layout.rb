2f692d534b92ac01a7ebdeb444605b4addca55c0
remi <remi@remitaylor.com>
2009-01-22 14:28:21 -0700

    wow, didn't think android's xml would be case sensitive!  a bit annoying to deal with ...

--- lib/android/layout.rb
+++ lib/android/layout.rb
@@ -13,7 +13,47 @@ class Android::Layout
   alias name file_name
 
   def render stylesheets
-    File.read file_path
+    layout_xml = File.read file_path
+    layout_xml = androidify_xml_attributes(layout_xml)
+  end
+
+  def androidify_xml_attributes xml
+    require 'hpricot'
+    doc = Hpricot(xml)
+    doc.traverse_element {|e|
+      if e.is_a?Hpricot::Elem
+        puts e.name
+        e.attributes.each do |k, v|
+          unless k.include?':'
+            puts "  #{ k.inspect }"
+            value = e.remove_attribute k
+            puts "value => #{value.inspect}"
+            puts "android:#{k}, #{value}"
+            puts "e.set_attribute 'android:#{k}', '#{value}'"
+            e.set_attribute "android:#{k}", "#{value}"
+          end
+        end
+      end
+    }
+    fix_case_sensitive_tags(doc.to_s)
+  end
+
+  def fix_case_sensitive_tags xml
+    {
+      'textcolor='     => 'textColor=',
+      '<linearlayout'  => '<LinearLayout',
+      '<textview'      => '<TextView',
+      '<button'        => '<Button',
+
+      # fix these ... should use regexp?
+      '</linearlayout'  => '</LinearLayout',
+      '</textview'      => '</TextView',
+      '</button'        => '</Button'
+
+    }.each do  |k, v|
+      xml.gsub! k, v
+    end
+    xml
   end
 
   def self.find_all directory
