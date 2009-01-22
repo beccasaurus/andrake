767b1e4c72a461a2bd4dfcecb983844f28b9337a
remi <remi@remitaylor.com>
2009-01-21 19:53:46 -0700

    added some helpers to specs so i can easily get example directory paths and whatnot

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,53 @@
-require File.dirname(__FILE__) + '/../lib/android'
-require File.dirname(__FILE__) + '/../lib/andrake'
+root = File.dirname(__FILE__) + '/../'
+require File.join(root, 'lib', 'android')
+require File.join(root, 'lib', 'andrake')
 require 'spec'
 
+class Array
+  def directories
+    select {|x| File.directory? x }
+  end
+  def files
+    select {|x| File.file? x }
+  end
+end
+
 Spec::Runner.configure do |config|
-  #
+
+  # Spec Helper methods
+  
+  def root
+    File.dirname(__FILE__) + '/../'
+  end
+
+  def example_dir
+    File.join root, 'examples'
+  end
+
+  def examples
+    Dir[ File.join(example_dir, '*/*') ].directories
+  end
+
+  def android_examples
+    Dir[ File.join(example_dir, 'android', '*') ].directories
+  end
+
+  def andrake_examples
+    Dir[ File.join(example_dir, 'andrake', '*') ].directories
+  end
+
+  def android_example name
+    File.join example_dir, 'android', name
+  end
+
+  def andrake_example name
+    File.join example_dir, 'andrake', name
+  end
+
+  def print_spec_helpers
+    %w( root example_dir examples android_examples andrake_examples ).each do |method|
+      puts "#{method} => #{send(method).inspect}"
+    end
+  end
+
 end
