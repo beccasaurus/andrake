93ec8082518cd8183362a32fbe95abee3138db8f
remi <remi@remitaylor.com>
2009-01-22 16:30:57 -0700

    got rid of res/values xml files

---
+++ examples/different_states_of_andrakeness/BasicUI_8/init.rb
@@ -0,0 +1,13 @@
+resources do |res|
+
+  res.colors  :white  => '#ffffff',
+              :red    => '#f00',
+              :green  => '#f0f0',
+              :yellow => '#ffffff00',
+              :blue   => '#0000ff'
+
+  res.strings :app_name => 'Andrake App',
+              :hello    => 'there',
+              :externalized_string => 'w00t'
+
+end
